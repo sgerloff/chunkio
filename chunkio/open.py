416bd0e09c822155f4dd7eaf710f1478015409ce
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2023-12-12 10:05:26 +0100

    update end-to-end test for writing chunked files

--- chunkio/open.py
+++ chunkio/open.py
@@ -1,6 +1,7 @@
 import builtins
 
 from typing import Optional
+from io import IOBase
 
 
 def open(*args, num_chunks: Optional[int] = None, **kwargs):
--- tests/test_write.py
+++ tests/test_write.py
@@ -6,25 +6,30 @@ import chunkio
 from chunkio.utils import get_chunk_file_path
 
 
-def test_write_num_chunks():
+def test_write_num_chunks_end_to_end():
     NUM_CHUNKS = 8
     NUM_LINES = 3
     NUM_TOTAL_LINES = NUM_LINES * NUM_CHUNKS
 
-    BASE_FILE_NAME = "/tmp/chunkio/write_fixed_file_number.txt"
+    # Initialize file location
+    TMP_DIR = "/tmp/chunkio"
+    BASE_FILE_NAME = os.path.join(TMP_DIR, "write_fixed_file_number.txt")
+    os.makedirs(TMP_DIR, exist_ok=True)
 
-    # Write File Chunk
-    with chunkio.open(BASE_FILE_NAME, num_chunks=8) as chunked_file:
-        for i in range(NUM_TOTAL_LINES):
+    # Write file chunk
+    input_data = list(range(NUM_TOTAL_LINES))
+    with chunkio.open(BASE_FILE_NAME, "w", num_chunks=8) as chunked_file:
+        for i in input_data:
             chunked_file.write(f"{i}\n")
 
     # Chunks are written to a directory
     pytest.assume(os.path.isdir(BASE_FILE_NAME))
+    pytest.assume(len(os.listdir(BASE_FILE_NAME)) == NUM_CHUNKS)
 
-    for i in range(NUM_CHUNKS):
-        chunk_file_path = get_chunk_file_path(BASE_FILE_NAME, i)
-        pytest.assume(os.path.isfile(chunk_file_path))
+    output_data = []
+    with chunkio.open(BASE_FILE_NAME, "r") as chunked_file:
+        for line in chunked_file:
+            output_data.append(int(line))
+    pytest.assume(input_data == output_data)
 
-        with open(chunk_file_path, "r") as file:
-            for line_number, line in enumerate(file):
-                pytest.assume( i + line_number * NUM_CHUNKS == int(line) )
+    os.removedirs(TMP_DIR)
