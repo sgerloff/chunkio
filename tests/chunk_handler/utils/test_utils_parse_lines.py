56b20d3703724270c8b34ffd8ce1fb67a6ee0484
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2023-12-21 10:20:53 +0100

    update parse line tests include linebreak and return simple list

--- tests/chunk_handler/utils/test_utils_parse_lines.py
+++ tests/chunk_handler/utils/test_utils_parse_lines.py
@@ -6,15 +6,17 @@ from chunkio.chunk_handler.utils import parse_lines
 
 
 @pytest.mark.parametrize(
-    "delimiter, input_string, expected_lines, expected_incomplete_line", [
-        ("\n", "line", [], "line"),
-        ("\n", "line\nline\n", ["line", "line"], ""),
-        ("\n", "line\nline", ["line"], "line"),
-        ("\n", "\nline\n\nline\n\nline", ["", "line", "", "line", ""], "line"),
-        ("<line break>",
-         "<line break>line<line break><line break>line<line break><line break>line",
-         ["", "line", "", "line", ""], "line")
+    "delimiter, input_string, expected_lines", [
+        ("\n", "line", ["line"]),
+        ("\n", "line\nline\n", ["line\n", "line\n", ""]),
+        ("\n", "line\nline", ["line\n", "line"]),
+        ("\n", "\nline\n\nline\n\nline", ["\n", "line\n", "\n", "line\n", "\n", "line"]),
+        (
+                "<line break>",
+                "<line break>line<line break><line break>line<line break><line break>line",
+                ["<line break>", "line<line break>", "<line break>", "line<line break>", "<line break>", "line"]
+        )
     ]
 )
-def test_parse_lines(delimiter: str, input_string: str, expected_lines: List[str], expected_incomplete_line: str):
-    assert parse_lines(input_string, delimiter=delimiter) == (expected_lines, expected_incomplete_line)
+def test_parse_lines(delimiter: str, input_string: str, expected_lines: List[str]):
+    assert parse_lines(input_string, delimiter=delimiter) == expected_lines
