be1fa9c91f5b8ba4233ddd17b9d8dd2391b99b3f
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2023-12-22 16:38:06 +0100

    implement interface function to list chunk files

---
+++ chunkio/list.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from chunkio.chunk_handler import SubdirNumberedChunkFormat
+
+
+def list_chunks(file_path: str,
+                keep_extension: bool = True,
+                index_format: str = "06d",
+                return_index: bool = False,
+                *_, **__):
+    chunk_format = SubdirNumberedChunkFormat(index_format=index_format, keep_extension=keep_extension)
+    return chunk_format.list(file_path, return_index=return_index)
